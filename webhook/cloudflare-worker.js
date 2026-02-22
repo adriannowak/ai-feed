958c9c1367fe3e67ea1b016a4f7695ca9be2ef1c
anowak <adrian@vector.org.pl>
2026-02-22 17:46:22 +0100

    Fixes

--- webhook/cloudflare-worker.js
+++ webhook/cloudflare-worker.js
@@ -64,7 +64,6 @@ function getAllowedIds(env) {
 async function sendMessage(env, chat_id, text) {
   console.log("Sending message to chat_id", chat_id, ":", text);
   let url = `https://api.telegram.org/bot${env.TELEGRAM_BOT_TOKEN}/sendMessage`
-  console.log(url);
   return fetch(
     url,
     {
